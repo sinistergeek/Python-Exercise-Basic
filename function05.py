2bb2b61bc7643a1c5d27bb28e397e7a0349183df
sinistergeek <susanbhattraidon@gmail.com>
2023-10-29 21:05:26 +0545

    function05

---
+++ function05.py
@@ -0,0 +1,7 @@
+def sum_prod(num1,num2):
+    num_sum = num1 + num2
+    num_prod = num1 * num2
+    return num_sum,num_prod
+x= int(input('Enter the first number :'))
+y= int(input('Enter the second number :'))
+print(sum_prod(x,y))
