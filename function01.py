fb3605b3977fbd387a1ef2a26a6cd492cffabcd6
sinistergeek <susanbhattraidon@gmail.com>
2023-10-29 20:23:09 +0545

    function01

---
+++ function01.py
@@ -0,0 +1,6 @@
+def hello_world_func(name,age,profession):
+    print('I am {} year old.'.format(age))
+    print('My name is {}.'.format(name))
+    print('I am {} by profession.'.format(profession))
+
+hello_world_func(32,'hacker','sinister')
