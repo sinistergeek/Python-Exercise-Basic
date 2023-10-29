a6dce2bfc7a3d3a40262d8fc31f41d950893fca0
sinistergeek <susanbhattraidon@gmail.com>
2023-10-29 20:58:50 +0545

    return00

---
+++ return00.py
@@ -0,0 +1,8 @@
+def adding_numbers(num1,num2,num3):
+    print('Have to add three numbers')
+    print('First Number = {}'.format(num1))
+    print('Second Number = {}'.format(num2))
+    print('Third Number = {}'.format(num3))
+    return num1 + num2 + num3
+x = adding_numbers(10,20,30)
+print('The function returned a value of {}'.format(x))
