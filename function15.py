1d9fdb0a8852783af6a0b8262545b28b818d22ab
sinistergeek <susanbhattraidon@gmail.com>
2023-10-30 21:12:10 +0545

    function15

---
+++ function15.py
@@ -0,0 +1,10 @@
+def outerWishes():
+    global wishes
+    wishes = "Happy New Year"
+def innerWishes():
+    globalwishes
+    wishes = "Have a great year ahead"
+    print('wishes =',wishes)
+wishes = "Happiness and Prosperity Always"
+outerWishes()
+print('wishes =', wishes)
