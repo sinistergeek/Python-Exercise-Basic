09130ab452c3c0542a9d71abbea3bf7ba5b9dedd
sinistergeek <susanbhattraidon@gmail.com>
2023-10-31 21:42:17 +0545

    threading00

---
+++ threading00.py
@@ -0,0 +1,7 @@
+import threading
+def thread_count(count):
+    print("I am the thread number ",count,".")
+for i in range(1,6):
+    t = threading.Thread(target = thread_count,args = (i,))
+    t.start()
+    t.join()
