dd96d76338896cce759081260c4e7f951f65cb86
sinistergeek <susanbhattraidon@gmail.com>
2023-11-01 18:03:24 +0545

    Matplot02

---
+++ Matplot02.py
@@ -0,0 +1,8 @@
+#import pyplot
+import matplotlib.pyplot as plt
+
+#plot the point with the help of plot(5,8) function, cyan color, marker #style hexagon
+plt.plot(5,8,'ch')
+#display the plot
+plt.show()
+
