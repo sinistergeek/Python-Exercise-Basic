27cb65731801e1f47aff6ba8dd430cde75d6fb7b
sinistergeek <susanbhattraidon@gmail.com>
2023-11-01 17:54:22 +0545

    Matplot00

---
+++ Matplot00.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+#define the values of x and y
+x= 5
+y= 8
+plt.plot(x,y)
+#plot the point with the help of plot(x,y) function
+plt.plot(x,y)
+#display the plot
+plt.show()
+
