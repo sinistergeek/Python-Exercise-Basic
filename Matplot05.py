72dee29448e0c6123c55f27478edc28a578112f6
sinistergeek <susanbhattraidon@gmail.com>
2023-11-01 18:10:09 +0545

    Matplot05

---
+++ Matplot05.py
@@ -0,0 +1,11 @@
+#import the pyplot module from matplotlib library
+import matplotlib.pyplot as plt
+
+#define two list for the values of x and y
+x = [10,20,30,40,50,60,70,80,90]
+y = [7,14,21,28,35,42,49,56,63]
+
+#Call the plot(x,y) function. Magenta color, circle points on dotted line
+plt.plot(x,y,"mo:")
+#Display the plot
+plt.show()
