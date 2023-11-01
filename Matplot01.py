95038b9de174e451c812c6eed7573259efa96e91
sinistergeek <susanbhattraidon@gmail.com>
2023-11-01 17:57:28 +0545

    Matplot01

---
+++ Matplot01.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+#define the values of x and y
+
+x = 5
+y = 8
+
+#plot the point with the help of plot(x,y) function, red color, marker style x
+plt.plot(x,y,'rx')
+#display the plot
+plt.show()
