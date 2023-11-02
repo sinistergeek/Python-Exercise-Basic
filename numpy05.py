57f2058e7426786e6393a8ab8ad91583d3d2c5a2
sinistergeek <susanbhattraidon@gmail.com>
2023-11-02 20:45:24 +0545

    numpy05

---
+++ numpy05.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = ['China','India','USA','Indonesia','Brazil','Bangladesh']
+y = [12323125,123123123,1231231,555,76767,8989898]
+plt.bar(x,y)
+plt.xlabel("Country")
+plt.ylabel("Population")
+plt.title("Country vs. Polulation")
+plt.show()
