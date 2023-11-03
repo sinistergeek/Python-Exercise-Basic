3e49d912f1f615a5c8dff0b793fda9fd7b1f1f6f
sinistergeek <susanbhattraidon@gmail.com>
2023-11-03 20:41:49 +0545

    Tkinter24

---
+++ Tkinter24.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+mw = tk.Tk()
+mw.geometry('200x200')
+lbl = tk.Label(mw,text="First label",fg="blue",bg="green",font=("Nibisoace",14,"bold italic"),width=200,height=200)
+lbl.pack()
+mw.mainloop()
