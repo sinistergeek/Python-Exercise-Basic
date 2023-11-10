64733f1ba7a412e359ac37a449d459d632edec00
sinistergeek <susanbhattraidon@gmail.com>
2023-11-10 22:09:30 +0545

    24

---
+++ 24.py
@@ -0,0 +1,13 @@
+class Person:
+
+    instances=[]
+    def __init__(self):
+        Person.instances.append(self)
+    @classmethod
+    def count_instances(cls):
+        return len(Person.instances)
+
+p1 = Person()
+p2 = Person()
+p3 = Person()
+print(Person.count_instances())
