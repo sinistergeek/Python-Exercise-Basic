1bfc83ddd91ae823b0736118967b459816532392
sinistergeek <susanbhattraidon@gmail.com>
2023-11-11 17:03:59 +0545

    35

---
+++ 35.py
@@ -0,0 +1,25 @@
+class Vector:
+    def __init__(self, *components):
+        self.components = components
+    def __repr__(self):
+        return f'Vector{self.components}'
+    def __str__(self):
+        return f'{self.components}'
+    def __len__(self):
+        return len(self.components)
+    def __bool__(self):
+        print(self.components)
+        if not self.components:
+            return False
+        print(self.components[0])
+        return False if not self.components[0] else True
+
+v1 = Vector()
+v2 = Vector(3,2)
+v3 = Vector(0,-3,2)
+v4 = Vector(5,0,-1)
+
+print(bool(v1))
+print(bool(v2))
+print(bool(v3))
+print(bool(v4))
