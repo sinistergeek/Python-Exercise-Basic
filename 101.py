4b1e03750310bba8312cee3e6075fe716932995d
sinistergeek <susanbhattraidon@gmail.com>
2023-11-13 21:12:46 +0545

    101

---
+++ 101.py
@@ -0,0 +1,11 @@
+class ClassOne(object):
+    def __init__(self,p1,p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def square(self,p3):
+        print(p3 ** 2)
+
+p = ClassOne(1,2)
+print(p.p1)
+p= ClassOne(1,2)
