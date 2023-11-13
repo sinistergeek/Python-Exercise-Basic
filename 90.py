4a679404e4b4cd2269ace5c889b36be8846fe2bf
sinistergeek <susanbhattraidon@gmail.com>
2023-11-13 15:50:24 +0545

    90

---
+++ 90.py
@@ -0,0 +1,8 @@
+def decimal_to_binary(number):
+    if number == 0:
+        return '0'
+    result = ''
+    while number > 0:
+        result += str(number % 2)
+        number = number //2
+        return result[::-1]
