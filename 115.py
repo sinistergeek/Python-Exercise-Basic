c54f3f2331f63a4482e2ba46e7581eec92583bd5
sinistergeek <susanbhattraidon@gmail.com>
2023-11-14 17:03:42 +0545

    115

--- 115.py
+++ 115.py
@@ -1,3 +1,14 @@
 dictionary = {'Anna':20,'Mark':15,'Clinton':18,'Alice':10}
 names = [*dictionary]
 print(names)
+
+arr_of_dicts = [
+    {
+        'id':num,
+        'name':name
+    } for num,name in enumerate(names,start=1)
+
+        ]
+
+
+print(arr_of_dicts)
