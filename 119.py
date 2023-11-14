26f6d197ca25bb8068b45deb21315c05a513b721
sinistergeek <susanbhattraidon@gmail.com>
2023-11-14 19:51:57 +0545

    119

--- 119.py
+++ 119.py
@@ -1,4 +1,6 @@
 x = "The days of Python 2 are almost over. Python 3 is the king now."
+if "z" in x or x.count("y") >= 2:
+    print("True")
 
 if type(x) is str and x.startswith("T"):
     print("True")
