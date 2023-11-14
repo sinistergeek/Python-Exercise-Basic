c8022b2c30521f977e5e2f8ad0fe23fd7ba0f8b7
sinistergeek <susanbhattraidon@gmail.com>
2023-11-14 19:54:16 +0545

    119

--- 119.py
+++ 119.py
@@ -4,7 +4,13 @@ if "z" in x or x.count("y") >= 2:
 
 if type(x) is str and x.startswith("T"):
     print("True")
+
 if x.index("t") < 10:
+    print("True!")
+else:
+    print("False!")
+
+if x[-3:].isdigit():
     print("True!")
 else:
     print("False!")
