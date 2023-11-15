1a878d15126a39b3065605642457886f8889fdd6
sinistergeek <susanbhattraidon@gmail.com>
2023-11-15 14:52:37 +0545

    138

---
+++ 138.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv) != 3:
+    exit(f"Usage: {sys.argv[0]} short-STRING long-STRING")
+
+string = sys.argv[1]
+text = sys.argv[2]
+
+if string in text:
+    loc = text.index(string)
+    print(string,"can be found in",text,"at",loc)
+else:
+    print(string,"can Not be found in",text)
