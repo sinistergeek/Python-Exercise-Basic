8eaf47ceba6067625930a53c39ff1562d1ab79a0
sinistergeek <susanbhattraidon@gmail.com>
2023-11-16 08:13:45 +0545

    154

---
+++ 154.py
@@ -0,0 +1,21 @@
+queue = []
+
+while True:
+    inp = input(":")
+    inp = inp.rstrip("\n")
+
+
+    if inp == 'x':
+        for name in queue:
+            print(name)
+        exit()
+    if inp == 's':
+        print(len(queue))
+        continue
+    if inp == 'n':
+        if len(queue) > 0:
+            print("Next is {}".format(queue.pop(0)))
+        else:
+            print("the queue is empty")
+        continue
+    queue.append(inp)
