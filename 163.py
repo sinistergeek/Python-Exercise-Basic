fcffb7bd976541ace68f74fea340afa7026549c4
sinistergeek <susanbhattraidon@gmail.com>
2023-11-16 17:22:38 +0545

    163

--- 163.py
+++ 163.py
@@ -5,3 +5,21 @@ print(answer)
 x = -3 
 answer = 'positive' if x > 0 else 'negative'
 print(answer)
+
+
+def main():
+    #length = 10
+    #width = 3
+    length = int(input('length: '))
+    width = int(input('Width:'))
+
+    if length <= 0:
+        print("length is not positive")
+        return
+    if width <= 0:
+        print("width is not positive")
+        return
+    area = length * width
+    print("The area is ", area)
+
+main()
