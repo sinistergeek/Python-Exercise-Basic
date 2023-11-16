34231f3c688ceff35a6a1ee07315a4057992b680
sinistergeek <susanbhattraidon@gmail.com>
2023-11-16 16:15:55 +0545

    160

---
+++ 160.py
@@ -0,0 +1,8 @@
+planets = ['Mercury','Venus','Earth','Mars','Juipter','Saturn']
+for idx,planet in enumerate(planets):
+    print(idx,planet)
+
+print('')
+enu = enumerate(planet)
+print(enu.__class__.__name__)
+print(enu)
