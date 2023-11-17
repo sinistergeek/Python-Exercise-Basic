c064e7dca05bb47b6e140519e37d04b4c7a93c8e
sinistergeek <susanbhattraidon@gmail.com>
2023-11-17 16:01:48 +0545

    170

---
+++ 170.py
@@ -0,0 +1,14 @@
+words = ['apple','banana0','peach','42']
+if 'apple' in words:
+    print("found apple")
+
+
+if 'a' in words:
+    print("found a")
+else:
+    print('Not found a')
+if '42' in words:
+    print('found 42')
+else:
+    print('Not found 42')
+
