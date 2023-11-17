3d577f978e3989ad85b691acb8a55a3419f6cfc5
sinistergeek <susanbhattraidon@gmail.com>
2023-11-17 16:12:36 +0545

    173

---
+++ 173.py
@@ -0,0 +1,12 @@
+names = ['Foo','kim','Jane','Bob','Bambi']
+print(names)
+names.append('Qux')
+print(names)
+
+planets = ['Mercury','Venus','Earth','Mars','Jupiter']
+print(planets)
+third = planets.pop(2)
+print(third)
+print(planets)
+
+jupyter_leanders=[]
