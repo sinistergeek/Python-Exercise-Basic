f70c394fd93d3de9120ac6f51b82c45d44827408
sinistergeek <susanbhattraidon@gmail.com>
2023-11-17 15:37:55 +0545

    169

---
+++ 169.py
@@ -0,0 +1,10 @@
+fields = ['one','two and three','four','five']
+
+together = ':'.join(fields)
+print(together)
+
+mixed = ' -=<>'.join(fields)
+print(mixed)
+
+another = ''.join(fields)
+print(another)
