1f30007f4a03982b809ca22edf055acaf110ec4a
sinistergeek <susanbhattraidon@gmail.com>
2023-11-18 17:27:00 +0545

    204

---
+++ 204.py
@@ -0,0 +1,7 @@
+english = set(['door','car','lunar','era'])
+spanish = set(['era','lunar','hola'])
+
+print('english: ',english)
+print('spanish: ',spanish)
+both = english.intersection(spanish)
+print(both)
