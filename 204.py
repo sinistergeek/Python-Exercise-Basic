c81ddf25368ecd869eaa236a57039c8aff345d54
sinistergeek <susanbhattraidon@gmail.com>
2023-11-18 17:45:40 +0545

    204

--- 204.py
+++ 204.py
@@ -3,7 +3,12 @@ spanish = set(['era','lunar','hola'])
 
 print('english: ',english)
 print('spanish: ',spanish)
+
 both = english.intersection(spanish)
+
 print(both)
 print('issubset: ', both.issubset(english))
 print('issubset: ', both.issubset(spanish))
+
+diff = english.symmetric_difference(spanish)
+print('symmetric_difference: ',diff)
