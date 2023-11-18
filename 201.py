ef9e4b1b1796e923f9c94c67fc15f43e8ed29e13
sinistergeek <susanbhattraidon@gmail.com>
2023-11-18 17:21:06 +0545

    201

--- 201.py
+++ 201.py
@@ -5,3 +5,14 @@ user ={
 
 for k in user:
     print("{} -> {}".format(k,user[k]))
+
+
+for k in user.keys():
+    user['email'] = 'foo@bar.com'
+    print(k)
+
+print('-----')
+
+for k in user:
+    user['birthdate'] = '1991'
+    print(j)
