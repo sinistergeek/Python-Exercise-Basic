8937f27378b85698a5257835c1352d836e00bcae
sinistergeek <susanbhattraidon@gmail.com>
2023-11-19 21:17:14 +0545

    249

---
+++ 249.py
@@ -0,0 +1,36 @@
+import sys
+
+def process(filename):
+    snmps = []
+    with open(filename) as fh:
+        for row in fh:
+            snmps.append({'orig':row.rstrip()})
+    print(snmps)
+
+    max_number_of_parts = 0
+    max_number_of_digits = 0
+    for snmp in snmps:
+        snmp['split'] = snmp['orig'].split('.')
+        max_number_of_parts = max(max_number_of_parts,len(snmp['split']))
+        for part in snmp['split']:
+            max_number_of_digits = max(max_number_of_digits,len(part))
+    padding = "{:0" + str(max_number_of_digits) + "}"
+    print(padding)
+    for snmp in snmps:
+        padded = []
+        padded_split = snmp['split'] + ['0'] + (max_number_of_parts - len(snmp['split\ ']))
+        for part in padded_split:
+            padded.append(padding.format(int(part)))
+        snmp['padded'] = padded
+        snmp['joined'] = '.'.join(padded)
+
+    snmps.sort(key = lambda e: e['joined'])
+    sorted_snmps = []
+    for snmp in snmps:
+        sorted_snmps.append(snmp['orig'])
+    for snmp in sorted_snmps:
+        print(snmp)
+
+if len(sys.argv) < 2:
+    exit("Usage: {} FILENAME".format(sys.argv[0]))
+process(sys.argv[1])
