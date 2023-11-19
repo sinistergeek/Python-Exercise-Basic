31fedf5aa8b953f484c7846bf1e81d4ac7d7f735
sinistergeek <susanbhattraidon@gmail.com>
2023-11-19 17:04:07 +0545

    229

---
+++ 229.py
@@ -0,0 +1,11 @@
+import re
+
+strings = ("axxxa","axxxxa","axxxxxa")
+
+for text in strings:
+    match = re.search(r'ax{4}',text)
+    if match:
+        print("Match")
+        print(match.group(0))
+    else:
+        print("Not Match")
