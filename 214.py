5569569cb8633813162b1e9873372dddd7e4c32e
sinistergeek <susanbhattraidon@gmail.com>
2023-11-19 08:14:57 +0545

    214

---
+++ 214.py
@@ -0,0 +1,23 @@
+def stats(*numbers):
+    total = 0
+
+    average = None
+    minx = None
+    maxx = None
+
+    for val in numbers:
+        total += val
+        if minx == None:
+            minx = maxx = val
+        if minx > val:
+            minx = val
+        if maxx < val:
+            maxx = val
+    if len(numbers):
+        average = total/len(numbers)
+    return total,average,minx,maxx
+ttl,avr,smallest,largest = stats(3,5,4)
+print(ttl)
+print(avr)
+print(smallest)
+print(largest)
