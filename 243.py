1c14116c96e756c40a30d759c41eeec23464ab4c
sinistergeek <susanbhattraidon@gmail.com>
2023-11-19 20:01:30 +0545

    243

---
+++ 243.py
@@ -0,0 +1,9 @@
+import re
+
+strings = ['banana','apple','infnite loop']
+
+for s in strings:
+    match = re.search(r'(.)\1',s)
+    if match:
+        print(match.group(0),'matched in',s)
+        print(match.group(1))
