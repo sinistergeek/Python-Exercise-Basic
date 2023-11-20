32708d10968f4b080056510e7f5a784030da35c5
sinistergeek <susanbhattraidon@gmail.com>
2023-11-20 08:14:38 +0545

    253

---
+++ 253.py
@@ -0,0 +1,10 @@
+import re
+
+filename = "phone.txt"
+
+with open(filename) as fh:
+    for line in fh:
+        match = re.search('''
+        \b(\d\d-\d{7} | \d\d\d-\d-\d{7}|\d\d-\d\d\d-\d\d\d\d)\b''',line,re.VERBOSE)
+        if match:
+            print(match.group(1))
