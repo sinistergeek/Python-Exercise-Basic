018ca2634470b96c08be3a06895464ec2f333faa
sinistergeek <susanbhattraidon@gmail.com>
2023-11-20 17:07:49 +0545

    267

---
+++ 267.py
@@ -0,0 +1,8 @@
+import signal
+
+def handler(signum,frame):
+    print('Signal handler called with signal',signum)
+signal.signal(signal.SIGINT,handler)
+username = input('Username:')
+print(username)
+
