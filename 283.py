117569174af32023b6338fd0f7ac285d61decd9a
sinistergeek <susanbhattraidon@gmail.com>
2023-11-21 08:08:34 +0545

    283

---
+++ 283.py
@@ -0,0 +1,17 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name')
+    parser.add_argument('--email')
+
+    print(type(parser).__name__)
+    print(parser.__class__)
+
+    print(parser.format_help())
+    parser.print_help()
+    return parser.parse_args()
+
+args = get_args()
+print(args.__class__)
+print(args.name)
