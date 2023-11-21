c6ce42db10ccaf61b612e8a4009ae3cc0e479ea8
sinistergeek <susanbhattraidon@gmail.com>
2023-11-21 16:51:09 +0545

    288

--- 288.py
+++ 288.py
@@ -2,5 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect("sample.db")
 c = conn.cursor()
-
+try:
+    c.execute('''CREATE TABLE companies(id PRIMARY KEY,name VARCHAR(100) UNIQUE NOT NULL,employees INTEGER DEfAULT 0)
+              ''')
+except sqlite3.OperationalError as e:
+    print('sqlite error',e.args[0])
+conn.commit()
 conn.close()
+print('done')
