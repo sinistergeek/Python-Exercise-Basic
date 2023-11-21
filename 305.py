65032017a96586af95f85d4e3e7435b305e1c3fd
sinistergeek <susanbhattraidon@gmail.com>
2023-11-21 20:55:27 +0545

    305

---
+++ 305.py
@@ -0,0 +1,28 @@
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server
+
+import time
+import cgi
+
+def hello_world(environ,start_response):
+    setup_testing_defaults(environ)
+
+    status = '200 OK'
+    headers = [('Content-type','text/html')]
+
+    start_response(status,headers)
+    form = cgi.FiledStorage(fp=environ['wsgi.input'],environ=environ)
+    if 'txt' in form:
+        return 'Echo: ' + form['txt'].value
+
+    return """
+    <form>
+    <input name="txt">
+    <input type="submit" value="Echo"/>
+    </form>
+    """
+
+httpd = make_server('',8000,hello_world)
+
+print("Serving on port 800...")
+httpd.serve_forever()
