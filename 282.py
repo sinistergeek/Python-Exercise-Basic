304c4164197661570636084981ec3f0a543df78b
sinistergeek <susanbhattraidon@gmail.com>
2023-11-21 08:03:36 +0545

    282

---
+++ 282.py
@@ -0,0 +1,14 @@
+import sys
+import module
+
+files = sys.argv[1:]
+
+for filename in files:
+    try:
+        module.read_and_divide(filename)
+    except ZeroDivisionError:
+        print("Cannot divide by 0 in file {}".format(filename))
+    except IOError:
+        print("Cannot open file {}".format(filename))
+    except ValueError as ex:
+        print("ValueError {} in file {}".format(filename))
