9b385b8d98cac7e2144b819b50fea4183b5cb045
sinistergeek <susanbhattraidon@gmail.com>
2023-11-22 16:49:00 +0545

    314

---
+++ 314.py
@@ -0,0 +1,8 @@
+import requests
+res = requests.get('http://localhost:5000/')
+print(res.status_code)
+print(res.text)
+
+res = requests.post('http://localhosts:5000/echo',data={"text": "Hello World!"})
+print(res.status_code)
+print(res.text)
