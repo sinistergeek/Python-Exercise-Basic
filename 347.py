31049c4092063e5febaf3772aa495da095f2b982
sinistergeek <susanbhattraidon@gmail.com>
2023-11-23 17:01:13 +0545

    347

---
+++ 347.py
@@ -0,0 +1,8 @@
+names = ["Helen","Ann","Mary","Harry","Joe","Peter"]
+names3 = filter(lambda w: len(w) == 3, names)
+
+loc3 = filter(lambda i : len(names[i])==3, range(len(names)) )
+print(list(loc3))
+
+has_e = filter(lambda i: "e" in names[i], range(len(names)))
+print(list(has_e))
