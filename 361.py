d19df263e629783e9788a2080b77b3ae091ce9fc
sinistergeek <susanbhattraidon@gmail.com>
2023-11-23 20:05:20 +0545

    361

---
+++ 361.py
@@ -0,0 +1,15 @@
+import sys
+
+table = {
+        "+" : lambda x,y : x+y,
+        "-" : lambda x,y : x-y,
+        "*" : lambda x,y : x*y,
+        "/" : lambda x,y : x/y,
+         }
+
+def main():
+    if len(sys.argv) != 4:
+        exit(f"Usage: {sys.argv[0]} NUMBER OF NUMBER")
+    action = table[sys.argv[2]]
+    print(action(int(sys.argv[1]),int(sys.argv[3])))
+main()
