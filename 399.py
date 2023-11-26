cbd0f43cde33790e029c6db2ba3d36d7a4c55fb0
sinistergeek <susanbhattraidon@gmail.com>
2023-11-26 17:57:56 +0545

    399

---
+++ 399.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
+
+logging.log(logging.WARNING,"another warning")
+logging.log(40,"another error")
