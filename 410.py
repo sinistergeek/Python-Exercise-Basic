04d8a359d60b1a0c9e76375032e9622cc4138ab4
sinistergeek <susanbhattraidon@gmail.com>
2023-11-27 14:21:51 +0545

    410

---
+++ 410.py
@@ -0,0 +1,12 @@
+import time
+def tron(func):
+    def new_func(*args,**kwargs):
+        start = time.time()
+        print("Calling {}{},{}".format(func.__name__,args,kwargs))
+        out = func(*args,**kwargs)
+        end = time.time()
+        print("Finished {}{}".format(func.__name__,out))
+        print("Elasped time: {}".format(end - start))
+        return out
+    return new_func
+
