def0af34e28a578449b4b55e3668fd7a62e076f0
sinistergeek <susanbhattraidon@gmail.com>
2023-11-27 16:32:52 +0545

    425

---
+++ 425.py
@@ -0,0 +1,14 @@
+names = ['Foo','Bar','Baz']
+
+ok = False
+
+for i in range(3):
+    name = input('Your name please:')
+    if name in names:
+        ok=True
+        break
+if not ok:
+    print("Not OK")
+    exit()
+
+print("OK.........")
