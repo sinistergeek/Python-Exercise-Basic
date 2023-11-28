f908cbcb1dbe1b69ec0b087016f3d119783e513c
sinistergeek <susanbhattraidon@gmail.com>
2023-11-28 20:59:20 +0545

    457

---
+++ 457.py
@@ -0,0 +1,8 @@
+class HouseProject:
+    number_of_floors = 3
+    area = 100
+
+    def describe_project():
+        print(f'Floor number:{HouseProject.number_of_floors}\nArea:{HouseProject.area}')
+
+HouseProject.describe_project()
