5a90346d1d4b89c0332f6d60ee26113489b5f03b
sinistergeek <susanbhattraidon@gmail.com>
2023-11-28 19:28:51 +0545

    453

---
+++ 453.py
@@ -0,0 +1,10 @@
+class OnlineShop:
+    sector = 'electronics'
+    sector_code = 'ELE'
+    is_public_company = False
+
+
+del OnlineShop.sector_code
+attrs = [attr for attr in OnlineShop.__dict__.keys() if not attr.startswith('_')]
+
+print(attrs)
