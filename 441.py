a24fba4747a3a4e273249d6e9e58c160ec6e367e
sinistergeek <susanbhattraidon@gmail.com>
2023-11-28 10:38:38 +0545

    441

--- 441.py
+++ 441.py
@@ -1,5 +1,17 @@
+i=0
 def display_info(company,**kwargs):
     print(f'Company name: {company}')
-    if 'price' in kwargs:
-        print(f"Price: ${kwargs['price']}")
-display_info(company='CD Project',price=100)
+    if i == 0:
+        
+        if 'price' in kwargs:
+            print(f"Price: ${kwargs['price']}")
+        else:
+            print("Enter the price")
+            a = int(input())
+            if a > 0:
+                display_info(company='SD POR',price=a)
+            else:
+                print("***** +ve ++++++++++")
+
+
+display_info(company='CD Project')
