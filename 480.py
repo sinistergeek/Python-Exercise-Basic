2f261b9e33d72ae1d5fbbcc8638aba6d7b2658f1
sinistergeek <susanbhattraidon@gmail.com>
2023-11-29 16:17:59 +0545

    480

---
+++ 480.py
@@ -0,0 +1,21 @@
+class Laptop:
+
+    def __init__(self,price):
+        self._price = price
+
+    def get_price(self):
+        return self._price
+
+    def set_price(self,value):
+        if not isinstance(value,(int,float)):
+            raise TypeError('The price attribute must be an int or float type')
+        if not value > 0:
+            raise ValueError('The price attribute must be positive int or float value')
+
+        self._price = value
+
+laptop = Laptop(3499)
+try:
+    laptop.set_price('-300')
+except TypeError as error:
+    print(error)
