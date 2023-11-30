650493d2c4dc60d921cd8da05ecf8bbb6dbadb97
sinistergeek <susanbhattraidon@gmail.com>
2023-11-30 20:20:06 +0545

    489

---
+++ 489.py
@@ -0,0 +1,15 @@
+class Pet:
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+pet = Pet('Max',4)
+print(pet.__dict__)
