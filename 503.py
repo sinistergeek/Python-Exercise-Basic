1a01f7bba92582bab86d0c440e0e649350ecc0dc
sinistergeek <susanbhattraidon@gmail.com>
2023-12-08 10:45:53 +0545

    503

---
+++ 503.py
@@ -0,0 +1,16 @@
+string = 'hello'
+
+substring = string[1:4]
+print(substring)
+
+
+nubstring = string[:3]
+print(nubstring)
+
+
+tubstring = string[3:]
+print(tubstring)
+
+lubstring = string[:]
+print(lubstring)
+
