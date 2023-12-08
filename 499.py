29c224756eb6ad8aa530ed304b2600329fef5dcd
sinistergeek <susanbhattraidon@gmail.com>
2023-12-08 08:06:18 +0545

    499

--- 499.py
+++ 499.py
@@ -1,7 +1,15 @@
 import keyword
 print(keyword.kwlist)
 
+
 x = """
 This is a multiline string that spans multiple lines
 """
 print(x)
+
+
+import sys
+
+print("Command line arguments:",sys.argv)
+print("First argument:", sys.argv[1])
+print("Second argument:", sys.argv[2])
