fdd453b74af44dcd537ef888f2f415d9c61610eb
sinistergeek <susanbhattraidon@gmail.com>
2023-12-08 17:00:21 +0545

    507

--- 507.py
+++ 507.py
@@ -8,3 +8,11 @@ print(sum)
 
 sum = add(1,2,3,4,5,6,7,8,9,10)
 print(sum)
+
+
+def greet(**kwargs):
+    for key,value in kwargs.items():
+        print(f'{key}:{value}')
+
+greet(name='Edcorner',greeting='Hello')
+
