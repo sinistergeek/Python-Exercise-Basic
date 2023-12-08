53825abe70fdd8e80f5dd66ede2959c91f48a57c
sinistergeek <susanbhattraidon@gmail.com>
2023-12-08 10:51:56 +0545

    504

---
+++ 504.py
@@ -0,0 +1,18 @@
+string = 'hello'
+
+string = string.upper()
+print(string)
+
+string = string.lower()
+print(string)
+
+string = string.replace('l','L')
+print(string)
+
+string = 'edcorner,learning,cherry'
+substrings = string.split(',')
+print(substrings)
+
+string = ' hello '
+string = string.strip()
+print(string)
