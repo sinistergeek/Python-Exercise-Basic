fe4e7e1eba93f248ca109658a3eaa1929363a63b
sinistergeek <susanbhattraidon@gmail.com>
2024-02-03 20:33:32 +0545

    527

---
+++ 527.py
@@ -0,0 +1,9 @@
+import functools
+def opsumcount(a,b):
+    return(b[0],a[1] + b[1])
+
+s = filter(lambda x : x not in ('a','the'),strings)
+m = map(len,s)
+length,total = functools.reduce(opsumcount,enumerate(m,1))
+average = total/length
+print(average)
