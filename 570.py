a5cc2074dbd87962cb50478df9afbd445300f4e2
sinistergeek <[susanbhattraidon@gmail.com]>
2024-02-28 10:02:13 +0545

    570

---
+++ 570.py
@@ -0,0 +1,12 @@
+class VsCode:
+    def functions(self):
+        print('compiler')
+        print('Execution')
+
+class laptop:
+    def code(self):
+        print('Here is the code')
+        dew = VsCode()
+        dew.functions()
+lap = laptop()
+lap.code()
