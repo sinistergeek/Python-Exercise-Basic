5f32f9567368faa46c723de822f4385fe15972ff
sinistergeek <susanbhattraidon@gmail.com>
2024-03-01 22:22:52 +0545

    593

---
+++ 593.py
@@ -0,0 +1,17 @@
+import math
+
+class Point:
+    def move(self, x:float,y:float) -> None:
+        self.x = x
+        self.y = y 
+
+    def reset(self) -> None:
+        self.move(0,0)
+
+    def calculate_distance(self,other:"Point") -> float:
+        return math.hypot(self.x - other.x, self.y - other.y)
+
+point = Point()
+point.x = 5
+print(point.x)
+print(point.y)
