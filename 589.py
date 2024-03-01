c40c69c62748107657e71235d04d2c1468f741ed
sinistergeek <susanbhattraidon@gmail.com>
2024-03-01 09:29:39 +0545

    589

---
+++ 589.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+
+wd.get("https://www.wikipedia.org/")
+assert "Wikipedia" in wd.title
+print(wd.page_source)
