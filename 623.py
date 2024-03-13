0320f6a7d41e196da0af9475e1e184e623b32e9b
sinistergeek <susanbhattraidon@gmail.com>
2024-03-13 14:25:22 +0545

    623

---
+++ 623.py
@@ -0,0 +1,9 @@
+def format_person(first,last,age):
+    return '{},{} - age {}'.format(last,first,age)
+
+first = ('John','Anne','Mary','Peter')
+last = ('Brown','Smith','Jones','Cooper')
+age = (25,33,41,28)
+
+m = map(format_person,firs,last,age)
+list(map(print,m))
