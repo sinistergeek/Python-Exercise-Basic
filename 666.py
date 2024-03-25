297f5af9ebb355f4410a02dc017f1de88c88fcbb
sinistergeek <susanbhattraidon@gmail.com>
2024-03-25 10:32:18 +0545

    666

---
+++ 666.py
@@ -0,0 +1,14 @@
+def shifty(b: int) -> int:
+    return 1 << b
+def multy(b:int) -> int:
+    if b == 0: return 1
+    return 2*multy(b-1)
+
+def faster(b:int) -> int:
+    if b == 0: return 1
+    if b%2 == 1: return 2*faster(b-1)
+    t= faster(b//2)
+    return t*t
+m1s = Mersenne1(shifty)
+m1m = Mersenne1(multy)
+m1f = Mersenne1(faster)
