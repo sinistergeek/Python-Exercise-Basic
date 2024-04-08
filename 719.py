6fd5bd03771a3b149964ef4774537af644d29825
sinistergeek <susanbhattraidon@gmail.com>
2024-04-08 08:36:49 +0545

    719

---
+++ 719.py
@@ -0,0 +1,10 @@
+from typing import Callable, Dict, List, TypeVar
+S_ = TypeVar("S_")
+K_ = TypeVar("K_")
+def partition(
+        key: Callable[[S_],K_],
+        data: Iterable[S_]) -> Dict[K_,List[S_]]:
+    dictionary: Dict[K_,List[S_]] = defaultdict(list)
+    for head in data:
+        dictionary[key(head)].append(head)
+    return dictionary
