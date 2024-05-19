9e3e0beb39d98bf02801fb303d443ff3910808b7
sinistergeek <susanbhattraidon@gmail.com>
2024-05-19 09:13:43 +0545

    794

---
+++ 794.py
@@ -0,0 +1,8 @@
+def calc_sum(sum_list):
+    smallest_value = min(num_list)
+    largest_value = max(num_list)
+    total = smallest_value + largest_value
+    return total
+numbers=[5,6,3,8,9]
+result = calc_sum(numbers)
+print(result)
