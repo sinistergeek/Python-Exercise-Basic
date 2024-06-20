b143ea10295802271debd161c84ac77881b2c69c
sinistergeek <susanbhattraidon@gmail.com>
2024-06-20 08:51:47 +0545

    827

--- 827.py
+++ 827.py
@@ -1,3 +1,20 @@
 os.chdir("c:\\mydir")
 cwd = os.getcwd()
-print("The current working directory is:",wd)
+print("The current working directory is:",cwd)
+
+c= char(86)
+print(c) 
+
+
+class Animal:
+    count = 88
+    def __init__(self,value1,value2):
+        self.name = value1 
+        self.age = value2 
+
+    def show(self):
+        print("The animal name is"+ self.name)
+        print("The tiger age is"+ self.age) 
+tiger = Animal("Tiger","100")
+tiger.show()
+print("Tiger counts"+str(tiger.count))
