98c20dfc448fb861987dd8f41e85cf91ca1e022a
sinistergeek <susanbhattraidon@gmail.com>
2024-06-24 09:13:51 +0545

    831

--- 831.py
+++ 831.py
@@ -3,15 +3,16 @@ for n in range(6):
         continue
     print(n)
 
-
 animals = ["ass","bat","cow"]
 animals.copy()
 print(animals)
-
-
 
 from item in cycle('abc'):
     print(item,end=' ')
     ch+=1
     if(ch==10):
         break
+
+import datetime 
+dt = datetime.datetime(2023,3,28)
+print(dt)
