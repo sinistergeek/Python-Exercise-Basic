29fbd85cb2160b05cafecc531b586c4c1508f6b6
sinistergeek <susanbhattraidon@gmail.com>
2024-06-27 08:13:05 +0545

    834

--- 834.py
+++ 834.py
@@ -5,3 +5,9 @@ print(removed_item)
 
 student = {"name":"Andy","age":"17","id":"0026"}
 print("The removed item is:"student.popitem())
+
+
+dict1 = {0:"apple",1:"banana",2:"cherry"}
+dict2 = {1:"berry",2:"cashew"}
+dict1.update(dit2)
+print("dict1 =",dict1)
