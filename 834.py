f9435545c2840b4304677be13fd8f3c7bf81e864
sinistergeek <susanbhattraidon@gmail.com>
2024-06-27 08:11:28 +0545

    834

--- 834.py
+++ 834.py
@@ -1,3 +1,7 @@
 myDict = {0:"apple",1:"banana",2:"cherry"}
 removed_item = myDict.pop(1)
 print(removed_item)
+
+
+student = {"name":"Andy","age":"17","id":"0026"}
+print("The removed item is:"student.popitem())
