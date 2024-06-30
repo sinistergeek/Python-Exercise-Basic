f5c4542337000e4ed5710a903c3a85732ef4dc87
sinistergeek <susanbhattraidon@gmail.com>
2024-06-30 08:00:29 +0545

    837

--- 837.py
+++ 837.py
@@ -3,13 +3,16 @@ p = {"name":"Andy","age":"18","id":"007"}
 j = json.dumps(p)
 print(j)
 
-
 from collections import Counter 
 obj = Counter("cheatsheet")
 for item in obj.elements():
     print(item)
 
-
 import demjson
 json = demjson.encode(dict)
+
+str = "Perl in 8 Hours!"
+s = str.endswith("!")
+print(s)
+
 
