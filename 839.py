199388fc298673dbd1a8a1fe81218092e1a9b97f
sinistergeek <susanbhattraidon@gmail.com>
2024-07-02 08:46:59 +0545

    839

--- 839.py
+++ 839.py
@@ -1,2 +1,10 @@
 f = open("myfile.txt","r")
 print(f.fileno())
+
+collection = [1,2,3,4,5,6,7,8,9,10]
+def func(number):
+    if number % 2 == 0:
+        return True 
+    return False 
+evenNum = list(filter(func,collection))
+print(evenNum)
