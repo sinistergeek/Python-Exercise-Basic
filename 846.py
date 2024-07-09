d59089c09465bd793158fa7f2c9389f650859fa5
sinistergeek <susanbhattraidon@gmail.com>
2024-07-09 08:48:15 +0545

    846.

--- 846.py
+++ 846.py
@@ -7,3 +7,18 @@ print(myString[7:16])
 str = "abec"
 print(str.index("e"))
 
+
+class Computer:
+    harddrive = 10000
+    memory = 8
+    def setValue(self,harddrive,memory):
+        Computer.hardrive = harddrive
+        Computer.memory = memory 
+class Desktop(Computer):
+    def capacity(self):
+        print("harddrive capcity: "+ str(self.harddrive))
+        print("Memory capcity: "+ str(self.memory))
+
+D = Desktop()
+D.setValue(900,7)
+D.capacity
