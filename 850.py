e3210c7ed7390b8f6b464b2185eeb4b9abe64db8
sinistergeek <susanbhattraidon@gmail.com>
2024-07-13 07:57:03 +0545

    850

--- 850.py
+++ 850.py
@@ -1,4 +1,8 @@
 set1 = {"ant","bee","cat"}
 set2 = {"ass","bat","cow"}
 unrelated = set1.isdisjoint(set2)
-print(unrelated1)
+print(unrelated)
+
+str = "100percent"
+s = str.isidentifier()
+print(s)
