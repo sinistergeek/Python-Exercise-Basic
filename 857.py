90b2119d0a14b41e7d643fc8e529e4ea116068b9
sinistergeek <susanbhattraidon@gmail.com>
2024-07-28 08:59:55 +0545

    857

--- 857.py
+++ 857.py
@@ -5,4 +5,17 @@ def fun1():
         v = "Rose"
     fun2()
     return v 
-print(fun1())
+print(fun1()
+
+class Animal:
+      count = 88 
+      def __init__(self,value1,value2):
+        self.name = value1 
+        self.age = value2 
+      def show(self):
+      print("The animal name is "+self.name)
+      print("The tiger age is "+self.age)
+
+tiger = Animal("Tiger","100")
+tiger.show()
+print("Tiger counts" + str(tiger.count))
