de859ad44686a6791c9cf3915f632de215f11537
sinistergeek <susanbhattraidon@gmail.com>
2024-08-01 10:04:06 +0545

    858

--- 858.py
+++ 858.py
@@ -5,3 +5,9 @@ print("The current working directory is:",cwd)
 os.chdir("c:\\mydir")
 cwd = os.chdir()
 print("The current working directory is:",cwd)
+
+import os
+path="/"
+contents = os.listdir(path)
+print("The directories and files in''', path,''':")
+print(contents)
