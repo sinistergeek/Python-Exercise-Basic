f11df7c3fbe595d8ef94557f44e3f4c00e23fa74
sinistergeek <susanbhattraidon@gmail.com>
2024-08-02 09:48:55 +0545

    858

--- 858.py
+++ 858.py
@@ -11,3 +11,18 @@ path="/"
 contents = os.listdir(path)
 print("The directories and files in''', path,''':")
 print(contents)
+
+class Computer:
+    def __init__(self,name):
+        self.name = name 
+    def capacity(self,harddrive,memory):
+        self.harddrive = harddrive 
+        self.memeory = memeory 
+class Laptop(Computer):
+    def capacity(self,harddrive,memory):
+        print(self.name)
+        print("Harddrive capcity:" + str(harddrive))
+        print("Memory capacity:" + str(memory))
+L = Laptop("Laptop")
+L.capcity(8000,6)
+
