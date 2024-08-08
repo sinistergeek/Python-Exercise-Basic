812620220b271705e364ab50825dffceb485312b
sinistergeek <susanbhattraidon@gmail.com>
2024-08-08 08:12:41 +0545

    859

--- 859.py
+++ 859.py
@@ -2,3 +2,15 @@ def apply_discount(product,discount):
     price = int(product['price'] * (1.0 - discount)) 
     assert 0 <= price <= product['price']
     return price
+
+
+if cond == 'x':
+    do_x()
+
+elif cond == 'y':
+    do_y()
+else:
+    assert False,(
+'This should never happen, but it does '
+'occassionally. We are currently trying to figure out why. Email dbadder is if you encounter this in the wild. Thanks'
+            )
