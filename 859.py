f02cab87b0cf475b7284f891bb2e5b52572c0a6b
sinistergeek <susanbhattraidon@gmail.com>
2024-08-11 09:19:43 +0545

    859

--- 859.py
+++ 859.py
@@ -19,3 +19,14 @@ def delete_product(prod_id,user):
     assert user.is_admin(),'Must be admin'
     assert store.has_product(prod_id),'Unknown product'
     store.get_product(prod_id).delete()
+
+class Indenter:
+    def __init__(self):
+        self.level = 0
+    def __enter__(self):
+        self.level += 1
+        return self 
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        self.level -= 1
+    def print(self,text):
+        print('     '*self.level + text)
