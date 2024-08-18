932255b9b63eb2d59288859a85c7a3b9771914af
sinistergeek <susanbhattraidon@gmail.com>
2024-08-18 08:24:09 +0545

    861

--- 861.py
+++ 861.py
@@ -1,3 +1,6 @@
 from string import Template 
 t = Template('Hey, $name!')
 t.substitute(name=name)
+
+templ_string = 'Hey $name, there is a $error error!'
+Template(templ_string).substitute(name=name,error=hex(errno))
