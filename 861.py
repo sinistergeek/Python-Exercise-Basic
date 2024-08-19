4758444fb55abdcc2cb4e3a0789e1b5c837d677a
sinistergeek <susanbhattraidon@gmail.com>
2024-08-19 09:57:20 +0545

    861

--- 861.py
+++ 861.py
@@ -4,3 +4,12 @@ t.substitute(name=name)
 
 templ_string = 'Hey $name, there is a $error error!'
 Template(templ_string).substitute(name=name,error=hex(errno))
+
+SECRET = 'this-is-a-secret'
+class Error:
+    def __init__(self):
+        pass 
+
+err = Error()
+user_input = '{error.__init__.__globals__[SECRET]}'
+user_input.format(error=err)
