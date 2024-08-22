605af6cade688bb96364cfece4ad30454964d3d9
sinistergeek <susanbhattraidon@gmail.com>
2024-08-22 08:43:18 +0545

    861

--- 861.py
+++ 861.py
@@ -41,3 +41,12 @@ def get_speak_func(volume):
 
 get_speak_func(0.3)
 get_speak_func(0.7)
+
+def make_adder(n):
+    def add(x):
+        return x + n 
+    return add 
+plus_3 = make_adder(3)
+plus_5 = make_adder(5) 
+plus_3(4) 
+plus_5(4)
