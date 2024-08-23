eb09105d3349b3c43de3398cde3c2011eb68f9de
sinistergeek <susanbhattraidon@gmail.com>
2024-08-23 08:16:01 +0545

    862

--- 862.py
+++ 862.py
@@ -12,3 +12,11 @@ get_speak_func('Hello,World',0.7)()
 
 add = lambda x,y: x + y
 add(5,3)
+
+def make_adder(n):
+    return lambda x: x + n 
+
+plus_3 =make_adder(3) 
+plus_5 =make_adder(5) 
+plus_3(4)
+plus_5(4)
