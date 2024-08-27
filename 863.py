ea420e679b9478ad6d981d288a7da44ffa275d47
sinistergeek <susanbhattraidon@gmail.com>
2024-08-27 07:53:39 +0545

    863

--- 863.py
+++ 863.py
@@ -7,3 +7,13 @@ def emphasis(func):
     def wrapper():
         return '<em>' + func() + '</em>'
     return wrapper
+
+def trace(func):
+    def wrapper(*args,**kwargs):
+        print(f'TRACE: calling {func.__name__}()'
+              f'with {args},{kwargs}')
+        original_result = func(*args,**kwargs)
+        print(f'TRACE: {func.__name__}()'
+              f'returned {original_result !r}')
+        return original_result
+    return wrapper
