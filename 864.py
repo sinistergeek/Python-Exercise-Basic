81e9fb0b450f3fe4f0643008bf21adcd6cb806b5
sinistergeek <susanbhattraidon@gmail.com>
2024-09-05 08:10:36 +0545

    864

--- 864.py
+++ 864.py
@@ -37,3 +37,17 @@ class Car:
 my_car = Car('red',3721)
 print(my_car)
 
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color 
+        self.mileage = mileage
+
+    def __repr__(self):
+        return '__repr__ for Car'
+
+    def __str__(self):
+        return '__str__ for Car'
+
+my_car = Car('red',37281)
+print(my_car)
+'{}'.format(my_car)
