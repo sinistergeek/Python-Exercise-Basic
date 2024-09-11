9c26bc1e768a5e4d08babcd023b602a09899c0bf
sinistergeek <susanbhattraidon@gmail.com>
2024-09-11 08:24:12 +0545

    864

--- 864.py
+++ 864.py
@@ -110,3 +110,13 @@ class Rectangle:
 
 rect = Rectangle(Point(0,1),Point(5,6))
 srect = copy.copy(rect)
+
+class Base:
+    def foo(self):
+        raise NotImplementedError()
+    def bar(self):
+        raise NotImplementedError()
+
+class Concrete(Base):
+    def foo(self):
+        return 'foo() called'
