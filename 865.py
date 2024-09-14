d7e67952bd93f05ca82aa8f41f27779259472d91
sinistergeek <susanbhattraidon@gmail.com>
2024-09-14 08:12:59 +0545

    865

--- 865.py
+++ 865.py
@@ -6,3 +6,15 @@ class MyCarWithMethods(Car):
             return '#ff0000'
         else:
             return '#000000'
+
+c = MyCarWithMethods('red',1234)
+c.hexcolor()
+
+class Dog:
+    num_legs = 4
+    def __init__(self,name):
+        self.name = name
+
+jack = Dog('Jack')
+jill = Dog('Jill')
+
