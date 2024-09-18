3e956850936cf9756c7aed6221fab78a3591140f
sinistergeek <susanbhattraidon@gmail.com>
2024-09-18 08:49:46 +0545

    865

--- 865.py
+++ 865.py
@@ -45,3 +45,17 @@ class Pizza:
         return f'Pizza({self.ingredients!r})'
 
 Pizza(['cheese','tomatoes'])
+
+
+class Pizza:
+    def __init__(self,ingredients):
+        self.ingredients = ingredients
+    def __repr__(self):
+        return f'Pizza({self.ingredients!r})'
+    @classmethod
+    def margherita(cls):
+        return cls(['mozzarella','tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['mozzarella','tomatoes','ham'])
