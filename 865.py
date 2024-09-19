6482a38a273b6ad31020907f560e3ca752ba8b4a
sinistergeek <susanbhattraidon@gmail.com>
2024-09-19 08:05:54 +0545

    865

--- 865.py
+++ 865.py
@@ -59,3 +59,21 @@ class Pizza:
     @classmethod
     def prosciutto(cls):
         return cls(['mozzarella','tomatoes','ham'])
+
+
+import math 
+
+class Pizza:
+    def __init__(self,radius,ingredients):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return(f'Pizza({self.raidus!r},'
+               f'{self.ingredients!r})')
+    def area(self):
+        return self.circle_area(self.radius)
+
+    @staticmethod
+    def circle_area(r):
+        return r ** 2 * math.pi
