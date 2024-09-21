c4f4cafe50b4ba18640027dd80c78b0e507cbf8b
sinistergeek <susanbhattraidon@gmail.com>
2024-09-21 09:06:06 +0545

    866

--- 866.py
+++ 866.py
@@ -3,3 +3,9 @@ dict1 = {'one':1,'two':2}
 dict2 = {'three':3,'four':4}
 chain = ChainMap(dict1,dict2)
 chain['three']
+
+from types import MappingProxyType
+writable = {'one':1,'two':2}
+read_only = MappingProxyType(writable)
+read_only['one']
+
