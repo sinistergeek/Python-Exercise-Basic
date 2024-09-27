7190c523f4013f08dafb4ba8a337b0d8e63a0662
sinistergeek <susanbhattraidon@gmail.com>
2024-09-27 10:27:27 +0545

    866

--- 866.py
+++ 866.py
@@ -37,3 +37,15 @@ Car = namedtuple('Car','color mileage automatic')
 car1 = Car('red',3812.4,True)
 print(car1)
 print(car1.mileage)
+
+
+from typing import NamedTuple
+
+class Car(NamedTuple):
+    color:str
+    mileage:float
+    automatic: bool
+
+car1 = Car('red',4323.4,True)
+print(car1)
+print(car1.mileage)
