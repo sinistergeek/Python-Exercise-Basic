8bd78a05263b527c5f1aca5cb8eb122b1ea1fd16
sinistergeek <susanbhattraidon@gmail.com>
2024-10-02 08:37:17 +0545

    867

--- 867.py
+++ 867.py
@@ -3,3 +3,9 @@ car1 = SimpleNamespace(color='red',
                        mileage=3812.4,
                        automatic=True)
 print(car1)
+
+from collections import Counter
+inventory = Counter()
+loot = {'sword':1,'bread':3}
+inventory.update(loot)
+print(inventory)
