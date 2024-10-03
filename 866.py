b04cdeba1dd03845e0548afa3b9a4813cf0b02c5
sinistergeek <susanbhattraidon@gmail.com>
2024-10-03 08:31:03 +0545

    866

--- 866.py
+++ 866.py
@@ -64,3 +64,11 @@ from struct import Struct
 MyStruct = Struct('i?f')
 data = MyStruct.pack(23,False,42.0)
 print(data)
+
+from collections import deque
+q = deque()
+q.append('eat')
+q.append('sleep')
+q.append('code')
+print(q)
+print(q.popleft())
