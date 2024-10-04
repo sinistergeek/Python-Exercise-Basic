a5403ef7c3d1d3bc3aa0377a66c12c287d6bf053
sinistergeek <susanbhattraidon@gmail.com>
2024-10-04 08:26:37 +0545

    867

--- 867.py
+++ 867.py
@@ -9,3 +9,13 @@ inventory = Counter()
 loot = {'sword':1,'bread':3}
 inventory.update(loot)
 print(inventory)
+
+import heapq
+q = []
+heapq.heappush(q,(2,'code'))
+heapq.heappush(q,(1,'eat'))
+heapq.heappush(q,(3,'sleep'))
+
+while q:
+    next_item = heapq.heappop(q)
+    print(next_item)
