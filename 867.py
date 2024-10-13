4df875a124b479f3c56eb7208fe95c9c3bb1c4e3
sinistergeek <susanbhattraidon@gmail.com>
2024-10-13 09:30:55 +0545

    867

--- 867.py
+++ 867.py
@@ -60,3 +60,9 @@ class Repeater:
         self.value = value
     def __iter__(self):
         return RepeaterIterator(self)
+
+class RepeaterIterator:
+    def __init__(self,source):
+        self.source = source
+    def __next__(self):
+        return self.source.value
