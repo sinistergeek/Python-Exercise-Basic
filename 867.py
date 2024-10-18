f8c78917cfa78dce685624376e54216488cdefb4
sinistergeek <susanbhattraidon@gmail.com>
2024-10-18 08:51:12 +0545

    867

--- 867.py
+++ 867.py
@@ -92,3 +92,18 @@ for n in numbers:
 
 my_list = [1,2,3]
 iterator = iter(my_list)
+
+class BoundedRepeater:
+    def __init__(self,value,max_repeats):
+        self.value = value
+        self.max_repeats = max_repeats
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.max_repeats:
+            raise StopIteration
+        self.count += 1
+        return self.value
