a33fcd92cd62ed9369ba8b25566536fabfe0d9fc
sinistergeek <susanbhattraidon@gmail.com>
2024-10-20 08:06:49 +0545

    867

--- 867.py
+++ 867.py
@@ -116,3 +116,13 @@ while True:
     except StopIteration:
         break
     print(item)
+
+class InfiniteRepeater(object):
+    def __init__(self,value):
+        self.value = value
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return self.value
+    def next(self):
+        return self.__next__()
