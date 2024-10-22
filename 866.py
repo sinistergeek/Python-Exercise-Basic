f3c9fdede6ec9378ac8dfa1b8199d2183774ed73
sinistergeek <susanbhattraidon@gmail.com>
2024-10-22 08:34:06 +0545

    866

--- 866.py
+++ 866.py
@@ -72,3 +72,17 @@ q.append('sleep')
 q.append('code')
 print(q)
 print(q.popleft())
+
+class BoundedRepeater:
+    def __init__(self,value,max_repeats):
+        self.value = value
+        self.max_repeats = max_repeats
+        self.count = 0
+
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.count >= self.maxt_repeats:
+            raise StopIteration
+        self.count += 1
+        return self.value
