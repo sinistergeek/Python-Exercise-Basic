9b0a4de8ffa1b1e2499bc4391959620d3195585e
sinistergeek <susanbhattraidon@gmail.com>
2024-10-25 08:44:45 +0545

    867

--- 867.py
+++ 867.py
@@ -126,3 +126,7 @@ class InfiniteRepeater(object):
         return self.value
     def next(self):
         return self.__next__()
+
+def bounded_repeater(value,max_repeats):
+    for i in range(max_repeats):
+        yield value
