8daf4d576c80a8394b8e6225256406902152d67e
sinistergeek <susanbhattraidon@gmail.com>
2024-10-31 10:35:37 +0545

    868

---
+++ 868.py
@@ -0,0 +1,12 @@
+name_for_userid = {
+        382: 'Alice',
+        950: 'Bob',
+        590: 'Dilbert',
+
+        }
+
+def greeting(userid):
+    return 'Hi %s!' %s name_for_userid[userid]
+
+greeting(382)
+greeting(33333)
