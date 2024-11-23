cb614ab10d47c8781d4344c379575f6576b4ccf2
sinistergeek <susanbhattraidon@gmail.com>
2024-11-23 09:49:30 +0545

    868

--- 868.py
+++ 868.py
@@ -123,3 +123,8 @@ print(name.title())
 name = "Ada Lovelace"
 print(name.upper())
 print(name.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f"{first_name}{last_name}"
+print(full_name)
