bd933388433f94a238d0769f7e47575e2a4caa47
sinistergeek <susanbhattraidon@gmail.com>
2024-11-29 08:11:12 +0545

    869

--- 869.py
+++ 869.py
@@ -2,3 +2,6 @@ motorcycles= ['honda','yamaha','suzuki','ducati']
 print(motorcycles)
 motorcycles.remove('ducati')
 print(motorcycles)
+
+first_owned = motorcycles.pop(0)
+print(f"The first  motorcycle I owned was a first_owned.title()}.")
