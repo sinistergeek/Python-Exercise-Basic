0dbfde77ed23f2a6f947d67e072e9bd095f06384
sinistergeek <susanbhattraidon@gmail.com>
2024-11-30 09:42:13 +0545

    869

--- 869.py
+++ 869.py
@@ -5,3 +5,7 @@ print(motorcycles)
 
 first_owned = motorcycles.pop(0)
 print(f"The first  motorcycle I owned was a first_owned.title()}.")
+
+cars = ['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
