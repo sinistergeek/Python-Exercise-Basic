33e2645618cd2228e1e19b61939fbaaca0be00f9
sinistergeek <susanbhattraidon@gmail.com>
2024-12-04 08:09:04 +0545

    869

--- 869.py
+++ 869.py
@@ -16,3 +16,9 @@ magicians = ['alice','david','carolina']
 for magician in magicians:
     print(f"{magician.title()},that was a great trick!")
 print(f"I can't wait to see your next trick, {magician,title()}.\n")
+
+squares = []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
