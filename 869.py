e0008567976296314660b571d36bb3323c2a89c8
sinistergeek <susanbhattraidon@gmail.com>
2024-12-06 08:45:49 +0545

    869

--- 869.py
+++ 869.py
@@ -25,3 +25,8 @@ print(squares)
 
 squares = [value**2 for value in range(1,11)]
 print(squares)
+
+players = ['charles','martina','michael','florence','eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
