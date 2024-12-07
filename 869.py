5cf3e2e87dd6630d651d1a8c01c7e520ff3e5808
sinistergeek <susanbhattraidon@gmail.com>
2024-12-07 14:48:23 +0545

    869

--- 869.py
+++ 869.py
@@ -30,3 +30,11 @@ players = ['charles','martina','michael','florence','eli']
 print("Here are the first three players on my team:")
 for player in players[:3]:
     print(player.title())
+
+my_foods = ['pizza','falafel','carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+
+print("\n My friend's favorite foods are:")
+print(friend_foods)
