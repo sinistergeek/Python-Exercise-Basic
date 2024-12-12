2297960c7deba58da0a874122172a8f2e45e0f9c
sinistergeek <susanbhattraidon@gmail.com>
2024-12-12 09:00:15 +0545

    869

--- 869.py
+++ 869.py
@@ -58,3 +58,8 @@ for car in cars:
             print(car.upper())
         else:
             print(car.title())
+
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
