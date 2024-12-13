67a61543a2eb9b497fd0440a2dda00fe5ac0d167
sinistergeek <susanbhattraidon@gmail.com>
2024-12-13 09:07:17 +0545

    869

--- 869.py
+++ 869.py
@@ -63,3 +63,7 @@ for car in cars:
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
+banned_users = ['andrew','carolina','david']
+user = 'marie'
+if user not in banned_users:
+    print(f"{user.title()},you can post a response if you wish.")
