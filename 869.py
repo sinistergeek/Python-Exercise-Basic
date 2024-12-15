a035b0217cfbcae124845a190ac0a737f5d7d8ea
sinistergeek <susanbhattraidon@gmail.com>
2024-12-15 08:10:47 +0545

    869

--- 869.py
+++ 869.py
@@ -72,3 +72,12 @@ if user not in banned_users:
 age = 19
 if age >=18:
     print("You are old enough to vote!")
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $20.")
+else:
+    print("Your admission cost is $40.")
+
