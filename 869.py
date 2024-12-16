622be5190ed65741560c98ff4ad01506ebb23aab
sinistergeek <susanbhattraidon@gmail.com>
2024-12-16 08:14:27 +0545

    869

--- 869.py
+++ 869.py
@@ -81,3 +81,12 @@ elif age < 18:
 else:
     print("Your admission cost is $40.")
 
+requested_toppings = ['mushrooms','extra cheese']
+if 'mushtrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
