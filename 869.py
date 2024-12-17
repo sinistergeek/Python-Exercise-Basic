f4f55cc18895f8eeec8eac1029019d1da4f40db5
sinistergeek <susanbhattraidon@gmail.com>
2024-12-17 08:53:05 +0545

    869

--- 869.py
+++ 869.py
@@ -90,3 +90,13 @@ if 'pepperoni' in requested_toppings:
 if 'extra cheese' in requested_toppings:
     print("Adding extra cheese.")
 print("\nFinished making your pizza!")
+
+available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
+
+requested_toppings = ['mushrooms','french fries','extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\n Finished making your pizza!")
