bda77748cf6b772e15f01e860ba212504fc84e43
sinistergeek <susanbhattraidon@gmail.com>
2024-12-19 08:15:48 +0545

    869

--- 869.py
+++ 869.py
@@ -104,3 +104,6 @@ print("\n Finished making your pizza!")
 alien_0 = {'color':'green','points':5}
 print(alien_0['color'])
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
