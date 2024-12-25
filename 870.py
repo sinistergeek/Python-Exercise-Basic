fc863cd3d9ec7592879c5186babc268eb457e685
sinistergeek <susanbhattraidon@gmail.com>
2024-12-25 08:49:01 +0545

    870

--- 870.py
+++ 870.py
@@ -6,3 +6,12 @@ favorite_languages ={
         }
 for name,language in favorite_languages,items():
     print(f"{name.title()}'s favorite language is {language.little()}.")
+
+
+friends = ['phil','sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()},I see you love {language}!")
