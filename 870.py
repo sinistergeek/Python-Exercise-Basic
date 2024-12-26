5fcc5a564da0dc36dd38e66be064fe18d116e04d
sinistergeek <susanbhattraidon@gmail.com>
2024-12-26 08:52:30 +0545

    870

--- 870.py
+++ 870.py
@@ -15,3 +15,16 @@ for name in favorite_languages.keys():
     if name in friends:
         language = favorite_languages[name].title()
         print(f"\t{name.title()},I see you love {language}!")
+
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color':'green','points':5,'speed':'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+
+print("...")
+
+print(f"Total number of aliens: {len(aliens)}")
