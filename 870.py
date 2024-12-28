647149d32222fb4a9e2d5d1d5648b7acfe0020a7
sinistergeek <susanbhattraidon@gmail.com>
2024-12-28 15:12:43 +0545

    870

--- 870.py
+++ 870.py
@@ -28,3 +28,15 @@ for alien in aliens[:5]:
 print("...")
 
 print(f"Total number of aliens: {len(aliens)}")
+
+favorite_languages = {
+'jen':['python','rust'],
+'sarah': ['c'],
+'edward':['rust','go'],
+'phil':['python','haskell'],
+        }
+
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
