398cb121c04fee8298c99328d55b63ecfdb4b37d
sinistergeek <susanbhattraidon@gmail.com>
2025-01-13 08:15:37 +0545

    870

--- 870.py
+++ 870.py
@@ -149,3 +149,9 @@ for name,response in responses.items():
 def greet_user(username):
     print(f"Hello, {username.title()}!")
 greet_user('jesse')
+
+def describe_pet(animal_type,pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster','harry')
