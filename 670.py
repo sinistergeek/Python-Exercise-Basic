f99c18d416128f1eaf812e72683684e9419c0dd8
sinistergeek <susanbhattraidon@gmail.com>
2025-01-15 08:00:58 +0545

    670

--- 670.py
+++ 670.py
@@ -21,3 +21,17 @@ def get_formatted_name(first_name,last_name):
     return full_name.title()
 musician = get_formatted_name('jimi','hendix')
 print(musician)
+
+def get_formatted_name(first_name,last_name,middle_name=''):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi','hendrix')
+print(musician)
+
+musician = get_formatted_name('john','hooker','lee')
+print(musician)
