54a41f0beed1cdae6a60d76b2df11ff4c8b66e05
sinistergeek <susanbhattraidon@gmail.com>
2025-01-17 08:50:36 +0545

    863

--- 863.py
+++ 863.py
@@ -65,3 +65,12 @@ genexpr = (expression for item in collection)
 def generator():
     for item in collection:
         yield expression
+
+
+def build_person(first_name,last_name):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name,'last': last_name}
+    return person
+
+musician = build_person('jimi','hendrix')
+print(musician)
