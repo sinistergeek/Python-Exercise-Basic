2f0686f6b4fc753827eb3ea48a888670676b04ca
sinistergeek <susanbhattraidon@gmail.com>
2025-01-23 09:02:22 +0545

    840

--- 840.py
+++ 840.py
@@ -5,3 +5,10 @@ import re
 pattern = re.compile(r'\d+')
 list = pattern.findall('ok23vary168good33',0,30)
 print(list)
+
+def make_pizza(*toppings):
+    """Print the list toppings that have been requested."""
+    print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms','green peppers','extra cheese')
