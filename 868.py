4c2018aff3ce58668e9b996d9003a15a93bf9d36
sinistergeek <susanbhattraidon@gmail.com>
2025-01-24 08:12:11 +0545

    868

--- 868.py
+++ 868.py
@@ -140,3 +140,11 @@ print(message)
 motorcycles = ['honda','yamaha','suzuki']
 motorcycles[0] = 'ducati'
 print(motorcycles)
+
+def build_profile(first,last,**user_info):
+    """Build a dictonary containing eerything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+user_profile = build_profile('albert','einstein',location='princeton',field='physics')
+print(user_profile)
