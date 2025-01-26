8324773476a61bb8dae0dbc29dd41cc133c8e195
sinistergeek <susanbhattraidon@gmail.com>
2025-01-26 08:15:45 +0545

    576

--- 576.py
+++ 576.py
@@ -6,3 +6,7 @@ if m:
     print("m.group(1,2):",m.group(1,2))
 else:
     print("No match!")
+
+import pizza as p
+p.make_pizza(16,'pepperoni')
+p.make_pizza(12,'mushrooms','green peppers','extra cheese')
