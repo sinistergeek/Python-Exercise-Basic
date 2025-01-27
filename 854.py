ecb4c557a1fd080de1ce0f0f69e9786dbb7203d3
sinistergeek <susanbhattraidon@gmail.com>
2025-01-27 14:37:44 +0545

    854

--- 854.py
+++ 854.py
@@ -25,3 +25,18 @@ while unprinted_designs:
 print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
+
+class Dog:
+    """ A simple attempt to model a dog."""
+
+    def __init__(self,name,age):
+        """ Intialize name and age attributes """
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
