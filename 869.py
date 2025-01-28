b316bc961c6c97190ac07d89aa0f861a3f33bf39
sinistergeek <susanbhattraidon@gmail.com>
2025-01-28 10:12:01 +0545

    869

--- 869.py
+++ 869.py
@@ -176,3 +176,7 @@ def greet_users(names):
         print(msg)
 usernames = ['hananah','ty','margot']
 greet_users(usernames)
+
+my_dog = Dog('Willie',6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
