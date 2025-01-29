cbb52c12909d3678687a49755ff554cd4883f18c
sinistergeek <susanbhattraidon@gmail.com>
2025-01-29 16:17:29 +0545

    867

--- 867.py
+++ 867.py
@@ -130,3 +130,20 @@ class InfiniteRepeater(object):
 def bounded_repeater(value,max_repeats):
     for i in range(max_repeats):
         yield value
+
+class Car:
+    """ A simple attempt to represent a car."""
+
+    def __init__(self,make,model,year):
+        """ Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_descriptive_name(self):
+        """ Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+my_new_car = Car('audi','a4',2024)
+print(my_new_car.get_descriptive_name())
