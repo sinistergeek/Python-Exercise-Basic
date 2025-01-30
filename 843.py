9b64f27f635c90aa16c836fe361e7a3d714b769f
sinistergeek <susanbhattraidon@gmail.com>
2025-01-30 08:11:01 +0545

    843

--- 843.py
+++ 843.py
@@ -19,3 +19,18 @@ def fun():
     myVar = "Cheat Sheet"
 fun()
 print("Html Css" + myVar)
+
+class Car:
+    def __init__(self,make,model,year):
+        """ Initialize attributes to describes a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def read_odometer(self):
+        """ Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+my_new_car = Car('audi','a4',2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
