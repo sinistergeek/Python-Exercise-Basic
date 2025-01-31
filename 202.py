ab1738b5065392b27b72a4a040d1416349dda5a8
sinistergeek <susanbhattraidon@gmail.com>
2025-01-31 09:01:40 +0545

    202

--- 202.py
+++ 202.py
@@ -4,3 +4,13 @@ if word not in counter:
     counter[word] = 0
 counter[word] += 1
 print(counter)
+
+class ElectricCar(Car):
+    """ Represent aspects of a car, specific to elelctric vehicles."""
+
+    def __init__(self,make,model,year):
+        """ Initialize attributes of the parent class."""
+        super().__init__(make,model,year)
+
+my_leaf = ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
