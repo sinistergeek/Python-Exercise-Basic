16bfee6e2658c02f8da7a36735a0101a33ddf137
sinistergeek <susanbhattraidon@gmail.com>
2025-02-02 07:59:05 +0545

    202

--- 202.py
+++ 202.py
@@ -24,3 +24,10 @@ class Battery:
     def describe_battery(self):
         """Print a statement describing the batteryt size."""
         print(f"This car has a  {self.battery_size}-kWh battery.")
+    def get_range(self):
+        """ Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+        print(f"This car can go about {range} miles on a full charge.")
