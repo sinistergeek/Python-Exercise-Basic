477b151023836d28bc4a777ca436a546b1528b5b
sinistergeek <susanbhattraidon@gmail.com>
2025-02-04 08:26:04 +0545

    204

--- 204.py
+++ 204.py
@@ -17,10 +17,15 @@ print('symmetric_difference: ',diff)
 all_the_words = english.union(spanish)
 print(all_the_words)
 
-from car import Car
+from car import Car, ElectricCar
 
 my_new_car = Car('audi','a4',2024)
 print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+my_mustang = Car('ford','mustang',2024)
+print(my_mustang.getdescriptive_name())
+my_leaf = ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
