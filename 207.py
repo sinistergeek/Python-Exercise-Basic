3285302c369db8dae6b60fd65f4a56e2fc7b9fcc
sinistergeek <susanbhattraidon@gmail.com>
2025-02-07 07:46:19 +0545

    207

--- 207.py
+++ 207.py
@@ -7,3 +7,12 @@ def prompt(questions, retry=3):
     return False
 print(prompt("Type in your password"))
 print(prompt("Type in your secret",1))
+
+from pathlib import Path
+import json
+
+numbers = [2,3,5,7,11,13]
+
+path = Path('numbers.json')
+contents = json.dumps(numbers)
+path.write_text(contents)
