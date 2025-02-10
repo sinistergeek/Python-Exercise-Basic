7a5f4a6aac953e7a2c3c43a7dc8da7a64bc33161
sinistergeek <susanbhattraidon@gmail.com>
2025-02-10 08:22:42 +0545

    702

--- 702.py
+++ 702.py
@@ -9,3 +9,14 @@ def convert(
     return(
             conversion(distance) for start, end, distance in trip
         )
+
+from pathlib import Path
+import json
+
+username = input("What is your name?")
+
+path = Path('username.json')
+contents = json.dumps(username)
+path.write_text(contents)
+
+print(f"we'll remember you whenn you come back, {username}!")
