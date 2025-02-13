dd4118a34c1965f03509bd62d8abfa168a77119e
sinistergeek <susanbhattraidon@gmail.com>
2025-02-13 08:36:40 +0545

    404

--- 404.py
+++ 404.py
@@ -16,3 +16,16 @@ log.info('info')
 log.warning('warning')
 log.error('error')
 log.critical('critical')
+
+from name_function import get_formattted_name
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name.")
+    if last  == 'q':
+        break
+    formatted_name = get_formatted_name(first,last)
+    print(f"\tNeatly formatted name:{formatted_name}.")
