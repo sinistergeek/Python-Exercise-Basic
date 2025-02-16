bb9b11960d9e82e46567acff8d752a03a15a2ab3
sinistergeek <susanbhattraidon@gmail.com>
2025-02-16 08:03:24 +0545

    205

--- 205.py
+++ 205.py
@@ -10,3 +10,9 @@ print(objects)
 objects.add('Neptun')
 print(objects)
 
+from survey import AnonymousSurvey
+
+def test_store_single_response():
+    question = "What language did you first learn to speak?"
+    language_survey.store_response("English")
+    assert 'English' in language_survey.responses
