40bc250d755ea19fb30662e213de1087f0ac0756
sinistergeek <susanbhattraidon@gmail.com>
2025-02-23 07:54:13 +0545

    869

--- 869.py
+++ 869.py
@@ -180,3 +180,14 @@ greet_users(usernames)
 my_dog = Dog('Willie',6)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
+
+def update(self):
+    """Move the bullet up the screen."""
+    #Update the exact position of the bullet.
+    self.y -= self.settings.bullet_speed
+    #Update the rect position.
+    self.rect.y = self.y
+
+def draw_bullet(self):
+    """Draw the bullet to the screen."""
+    pygame.draw.rect(self.screen,self.color,self.rect)
